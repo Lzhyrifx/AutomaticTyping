c6e534356b02af4fcf00f04015c9b0e782ec9374
Lzhyrifx <65514984+Lzhyrifx@users.noreply.github.com>
2022-09-25 16:19:50 +0800

    Add files via upload

---
+++ Colony.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.edge.service import Service
+
+driver = webdriver.Edge(service=Service("Z:\Project\Python\Python39\msedgedriver.exe"))
+driver.maximize_window()
+driver.get('https://dazi.91xjr.com/')
+driver.find_element(by=By.XPATH, value='//*[@id="dom-indexTabs"]/li[2]/a').click()
+driver.find_element(by=By.XPATH, value='//*[@id="dom-indexPnls"]/div[2]/form/div[3]/button').click()
+for z in range(114514):
+    text = driver.find_element(by=By.ID, value=f'box_{z}').text
+    driver.find_element(by=By.XPATH, value='''//*[@id="box_''' + str(z) + '''"]/input[2]''').send_keys(text, Keys.SPACE)
---
+++ Individual.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.edge.service import Service
+
+driver = webdriver.Edge(service=Service("Z:\Project\Python\Python39\msedgedriver.exe"))
+driver.maximize_window()
+driver.get('https://dazi.kukuw.com/')
+driver.find_element(by=By.NAME, value='start_button').click()
+for z in range(114514):
+    text = driver.find_element(by=By.ID, value=f'i_{z}').text
+    driver.find_element(by=By.XPATH, value='''//*[@id="i_''' + str(z) + '''"]/input[2]''').send_keys(text, Keys.SPACE)
