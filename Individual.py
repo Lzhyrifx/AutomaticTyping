c5bdca0c3f70f55e20b51812a0a658546c62d631
Lzhyrifx <65514984+Lzhyrifx@users.noreply.github.com>
2023-02-22 01:56:07 +0800

    Update Individual.py

--- Individual.py
+++ Individual.py
@@ -7,6 +7,6 @@ driver = webdriver.Edge(service=Service("Z:\Project\Python\Python39\msedgedriver
 driver.maximize_window()
 driver.get('https://dazi.kukuw.com/')
 driver.find_element(by=By.NAME, value='start_button').click()
-for z in range(114514):
+for z in range(10086):
     text = driver.find_element(by=By.ID, value=f'i_{z}').text
     driver.find_element(by=By.XPATH, value='''//*[@id="i_''' + str(z) + '''"]/input[2]''').send_keys(text, Keys.SPACE)
