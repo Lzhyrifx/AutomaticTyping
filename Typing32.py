64dd19085ec3c0c837c6e11e3805f87ed8a8f0ef
Lzhyrifx <65514984+Lzhyrifx@users.noreply.github.com>
2023-02-22 23:25:03 +0800

    Update Typing32.py

--- Typing32.py
+++ Typing32.py
@@ -2,10 +2,10 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Firefox()
-driver.maximize_window()
+driver = webdriver.Firefox()  # Firefox驱动
+driver.maximize_window()  # 网页最大化
 time.sleep(10)
-driver.get('http://10.189.240.90/')
+driver.get('')  # 跳转到指定网页
 time.sleep(30)
 driver.find_element(by=By.ID, value='Button2').click()
 while True:
